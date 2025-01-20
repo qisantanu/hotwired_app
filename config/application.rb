2255613a03062d7d4a918c58fc5f65c596747ff0
Santanu Bhattachrya <santanu.bhattacharya@quantuminventions.com>
2025-01-21 00:01:20 +0530

    feat: enhance UI and pwa

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-if Rails.env.production?
+if Rails.env.development?
   Dotenv.load '.env.dev'
 end
 
